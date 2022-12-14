4b17caa34586b1bd976f7f0d3fabacbb8a4e45d4
codeBlack5 <stevekoech8@gmail.com>
2022-12-14 12:36:44 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let newForm = document.getElementById("create-task-form")
+  newForm.addEventListener("submit", submitClick)
+  let newList = document.getElementById("tasks")
+  newList.addEventListener('click', deleteToDo)
+})
+
+
+
+function submitClick(event) {
+  event.preventDefault()
+
+  let newDescription = document.getElementById("new-task-description")
+  let value = newDescription.value
+  let newList = document.getElementById("tasks")
+  let newLi = document.createElement('li')
+  newLi.innerText = value
+  newList.appendChild(newLi)
+
+
+  let button = document.createElement("button")
+  button.innerText = "d"
+  newLi.append(button)
+  newDescription.value = 'element created'
+
+
+}
+
+
+function deleteToDo(event) {
+  console.log(event)
+  event.target.parentElement.remove()
+}
